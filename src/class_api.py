c33011e82d30640d903e15882a1591c78143bb69
AleksandrStrulkov <astrulkov@yandex.ru>
2023-10-03 23:52:36 +0300

    Add classes in files

--- src/class_api.py
+++ src/class_api.py
@@ -1,6 +1,13 @@
 from abc import ABC, abstractmethod
 
 
+class API(ABC):
+	pass
 
 
+class HeadHunter_API(API):
+	pass
 
+
+class SuperJob_API(API):
+	pass
--- src/class_userinput.py
+++ src/class_userinput.py
@@ -1,9 +1,17 @@
+import copy
+from src.class_api import HeadHunter_API, SuperJob_API
+from src.class_mylist import Mylist
 
 
 class UserInput:
-
+	param_zero = {}
 	def __init__(self):
-		pass
+		self.hh_api = HeadHunter_API()
+		self.sj_api = SuperJob_API()
+		self.mylist = Mylist()
+		self.all_list = Mylist()
+		self.param = copy.deepcopy(self.param_zero)
+
 
 	def __call__(self):
 		pass
