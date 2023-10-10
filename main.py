b8f27729258b11bd0e5114b7017020dca413e50d
AleksandrStrulkov <astrulkov@yandex.ru>
2023-10-10 22:05:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from src.class_userinput import UserInput
+from src.class_api import HeadHunter_API
+from pprint import pprint
 
 if __name__ == '__main__':
-	userinput = UserInput()
-	UserInput()
+	# user_input = UserInput()
+	# UserInput()
+	hh_api = HeadHunter_API("Москва", "Водитель")
+	data = hh_api.get_vacancies()
+	pprint(data, indent=4)
+	pprint(len(data))
