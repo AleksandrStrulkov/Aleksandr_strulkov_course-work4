106ec95d1c98c1b91fd7497889478c153aebeea1
AleksandrStrulkov <astrulkov@yandex.ru>
2023-10-15 08:44:26 +0300

    Валидация данных с HH в классе Vacancy ок

--- src/class_vacancy.py
+++ src/class_vacancy.py
@@ -1,78 +1,68 @@
 class Vacancy:
 	def __init__(self, vacancy_info: dict):
-		self.__vacancy_info = vacancy_info
-		# self.id = vacancy_info['id']
-		self.type = vacancy_info['type']['name']
-		self.name = vacancy_info['name']
-		self.salary_from = vacancy_info['salary_from']
-		self.salary_to = vacancy_info['salary_to']
-		self.area = vacancy_info['area']['name']
-		self.url = vacancy_info['url']
-		self.schedule = vacancy_info['schedule']['name']
-		self.experience = vacancy_info['experience']['name']
+		self.vacancy_info = vacancy_info
 
-def vacancy_valid_hh(vacancy_inf):
-	vacancy_valid_hh = []
-	for vacancy in vacancy_inf:
-		if isinstance(vacancy["id"], str):
-			vacancy_id = vacancy["id"]
+	def vacancy_valid_hh(self):
+		"""Валидация данных с вакансий с платформы HeadHunter"""
+		hh_valid_vacancy = []
+		for vacancy in self.vacancy_info:
+			if isinstance(vacancy["id"], str) and vacancy["id"].isdigit():
+				vacancy_id = vacancy["id"]
 
-		if isinstance(vacancy["type"]["name"], str):
-			vacancy_type = vacancy["type"]["name"]
-
-		if isinstance(vacancy["name"], str):
+			if isinstance(vacancy["name"], str):
 				vacancy_name = vacancy["name"]
 
-		if vacancy['salary'] is not None:
-			vacancy_salary_from = vacancy['salary']['from'] = "Информация скрыта"
+			if vacancy['salary']['from'] is not None and isinstance(vacancy['salary']['from'], int):
+				vacancy_salary_from = vacancy['salary']['from']
+			else:
+				vacancy['salary']['from'] = "Информация отсутствует"
+				vacancy_salary_from = vacancy['salary']['from']
 
-		if vacancy['salary'] is not None:
-			vacancy_salary_to = vacancy['salary']['to'] = "Информация скрыта"
+			if vacancy['salary']['to'] is not None and isinstance(vacancy['salary']['to'], int):
+				vacancy_salary_to = vacancy['salary']['to']
+			else:
+				vacancy['salary']['to'] = "Информация отсутствует"
+				vacancy_salary_to = vacancy['salary']['to']
 
-		if isinstance(vacancy['area']['name'], str) and vacancy['area']['name'] is not None:
-			vacancy_area = vacancy['area']['name']
+			if isinstance(vacancy['area']['name'], str):
+				vacancy_area = vacancy['area']['name']
 
-		else:
-			vacancy_area = False
+			if "https://" in vacancy["alternate_url"]:
+				vacancy_url = vacancy["alternate_url"]
 
-		if "https://" in vacancy["alternate_url"]:
-			vacancy_url = vacancy["alternate_url"]
+			if isinstance(vacancy["schedule"]["name"], str):
+				vacancy_schedule = vacancy["schedule"]["name"]
 
-		if isinstance(vacancy["schedule"]["name"], str) and vacancy["schedule"]["name"] is not None:
-			vacancy_schedule = vacancy["schedule"]["name"]
+			if isinstance(vacancy['experience']['name'], str):
+				vacancy_experience = vacancy['experience']['name']
 
-		else:
-			vacancy_schedule = False
+			if (vacancy_id and vacancy_name
+				and vacancy_salary_from
+				and vacancy_salary_to
+				and vacancy_area
+				and vacancy_url
+				and vacancy_schedule
+				and vacancy_experience):
+				hh_valid_vacancy.append(vacancy)
 
-		if isinstance(vacancy['experience']['name'], str) and vacancy['experience']['name'] is not None:
-			vacancy_experience = vacancy["schedule"]["name"]
-		else:
-			vacancy_experience = False
+		return hh_valid_vacancy
 
-		if (vacancy_id and vacancy_type and vacancy_name and vacancy_salary_from and vacancy_salary_to and
-				vacancy_area and vacancy_url and vacancy_schedule and vacancy_experience):
+	def readable_view_vacancy_hh(self):
+		"""Метод преобразования в читаемый вид вакансий с платформы HeadHunter"""
+		hh_vacancy_readable = []
+		for vacancy in self.vacancy_valid_hh():
+			vacancies_items = {
+					"id вакансии": vacancy['id'],
+					"Наименование вакансии": vacancy['name'],
+					"Минимальная зарплата": vacancy['salary']['from'],
+					"Максимальная зарплата": vacancy['salary']['to'],
+					"Город": vacancy['area']['name'],
+					"Ссылка на вакансию": vacancy['url'],
+					"Расписание работы": vacancy['schedule']['name'],
+					"Опыт работы": vacancy['experience']['name']
+			}
+			hh_vacancy_readable.append(vacancies_items)
+		return hh_vacancy_readable
 
-			vacancy_valid_hh.append(vacancy)
-	return vacancy_valid_hh
-
-	def vacancy_valid_sj(self):
-		pass
-
-
-def readable_view_vacancy_hh(vacancy_readable):
-	"""Метод преобразования в читаемый вид"""
-	hh_vacancy_readable = []
-	for vacancy in vacancy_readable:
-		vacancies_items = {
-				"id вакансии": vacancy["id"],
-				"Тип вакансии": vacancy["type"]["name"],
-				"Наименование вакансии": vacancy["name"],
-				"Минимальная зарплата": vacancy["salary"]["from"],
-				"Максимальная зарплата": vacancy["salary"]["to"],
-				"Город": vacancy["area"]["name"],
-				"Ссылка на вакансию": vacancy["url"],
-				"Расписание работы": vacancy["schedule"]["name"],
-				"Опыт работы": vacancy["experience"]["name"]
-		}
-		hh_vacancy_readable.append(vacancies_items)
-	return hh_vacancy_readable
+	def get_vacancies_items(self):
+		return self.vacancy_info
